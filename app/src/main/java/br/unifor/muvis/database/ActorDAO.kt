702236dc6ef4aba76bf7988977a6c193aef8a070
Mateus <mateusg.sales@gmail.com>
2017-11-01 08:46:16 -0300

    second commit

--- app/src/main/java/br/unifor/muvis/database/ActorDAO.kt
+++ app/src/main/java/br/unifor/muvis/database/ActorDAO.kt
@@ -8,14 +8,22 @@ import br.unifor.muvis.entity.Actor
 /**
  * Created by mateu on 01/11/2017.
  */
-class ActorDAO(context: Context) :GenericDAO<Actor>(context, "Actors") {
+class ActorDAO(context: Context) : GenericDAO<Actor>(context, "Actors") {
 
     override fun getContentValues(obj: Actor): ContentValues {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val values = ContentValues()
+        values.put("name",obj.name)
 
+        return values
     }
 
     override fun getModelByCursor(queryResult: Cursor): Actor {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val id = queryResult.getLong(queryResult.getColumnIndex("_id"))
+        val name = queryResult.getString(queryResult.getColumnIndex("name"))
+
+        //return Actor()
     }
 }
--- app/src/main/java/br/unifor/muvis/database/MovieDAO.kt
+++ app/src/main/java/br/unifor/muvis/database/MovieDAO.kt
@@ -3,6 +3,7 @@ package br.unifor.muvis.database
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
+import br.unifor.muvis.entity.Director
 import br.unifor.muvis.entity.Movie
 
 /**
@@ -12,11 +13,22 @@ class MovieDAO(context: Context) : GenericDAO<Movie>(context, "movies"){
 
     override fun getContentValues(obj: Movie): ContentValues {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val values = ContentValues()
+        values.put("name",obj.name)
+        values.put("year", obj.year)
+
+        return values
+
     }
 
     override fun getModelByCursor(queryResult: Cursor): Movie {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val id = queryResult.getLong(queryResult.getColumnIndex("_id"))
+        val name = queryResult.getString(queryResult.getColumnIndex("name"))
+        val year = queryResult.getInt(queryResult.getColumnIndex("year"))
+        val Director = queryResult.getLong(queryResult.getColumnIndex("Director"))
+
+        //return Movie()
     }
-
-
 }
